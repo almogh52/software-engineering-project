45b7937581940efcb38a3384bd97a49ea969cb2f
Almog Hamdani <almogh52@gmail.com>
2019-11-30 18:52:40 +0200

    feat: implement a basic utils class that checks if object is null or empty

---
+++ CSCProject/Misc/Utils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCProject.Misc
+{
+    public static class Utils
+    {
+        public static bool IsAnyNullOrEmpty(object myObject)
+        {
+            if (myObject == null)
+            {
+                return false;
+            }
+
+            foreach (PropertyInfo pi in myObject.GetType().GetProperties())
+            {
+                object value = pi.GetValue(myObject);
+
+                if (pi.PropertyType == typeof(string))
+                {
+                    if (string.IsNullOrEmpty((string)value))
+                    {
+                        return true;
+                    }
+                } else
+                {
+                    if (IsAnyNullOrEmpty(value))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
