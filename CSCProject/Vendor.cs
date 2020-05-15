a5d72d0844af3b1b56461b86012175a2874656f9
Almog Hamdani <almogh52@gmail.com>
2020-05-15 03:52:35 +0300

    fix: fix property typo in model

--- CSCProject/Vendor.cs
+++ CSCProject/Vendor.cs
@@ -17,7 +17,7 @@ namespace CSCProject
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Vendor()
         {
-            this.purchase_order = new HashSet<PurchaseOrder>();
+            this.PurchaseOrders = new HashSet<PurchaseOrder>();
         }
     
         public int Id { get; set; }
@@ -28,6 +28,6 @@ namespace CSCProject
     
         public virtual Address Address { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<PurchaseOrder> purchase_order { get; set; }
+        public virtual ICollection<PurchaseOrder> PurchaseOrders { get; set; }
     }
 }
